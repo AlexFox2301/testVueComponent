0e9fe5f43bb5045f17aa0ce72a746385b2d7f52c
Artyom <talanov1984@mail.ua>
2019-04-18 23:10:39 +0300

    Initial commit

---
+++ src/Car.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Car</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, esse!</p>
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+  }
+
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid black;
+  }
+</style>
---
+++ src/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Counter</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, voluptatum.</p>
+  </div>
+</template>
+
+<script>
+  import {eventEmitter} from './main';
+
+  export default{
+
+  }
+</script>
+
+<style scoped>
+  h2 {
+    color: red;
+  }
+</style>
